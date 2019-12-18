059c2fb290d8e0bed4d352f6a1d537b36d778265
steve <chph13420146901@gmail.com>
2019-12-18 12:06:00 +0800

    [feature] use 10*10px images as placeholder for img

--- homework3/static/js/index.js
+++ homework3/static/js/index.js
@@ -11,13 +11,13 @@ window.onload = () => {
         //     });
 
         images[i].setAttribute('src', `/assets/screenshots/progressive/${i + 1}.jpg`);
-        images[i].addEventListener('loadstart', () => {
-            // 只有火狐支持，chrome不支持，不知道为啥
-            images[i].style.padding = '0';
-        });
-        images[i].addEventListener('load', function(e) {
-            // 避免有些浏览器不支持loadstart事件
-            images[i].style.padding = '0';
-        });
+        // images[i].addEventListener('loadstart', () => {
+        //     // 只有火狐支持，chrome不支持，不知道为啥
+        //     images[i].style.padding = '0';
+        // });
+        // images[i].addEventListener('load', function(e) {
+        //     // 避免有些浏览器不支持loadstart事件
+        //     images[i].style.padding = '0';
+        // });
     }
 };
