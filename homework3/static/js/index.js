9b6f13f49752cc8a9af053be8e01c1703c12bab5
steve <chph13420146901@gmail.com>
2019-12-18 11:13:53 +0800

    [feature] set img src asynchronously

--- homework3/static/js/index.js
+++ homework3/static/js/index.js
@@ -1,3 +1,15 @@
 window.onload = () => {
-
+    const images = document.getElementsByClassName("screenshot-item-img");
+    for (let i = 0; i < images.length; i++) {
+        // fetch(`/assets/screenshots/progressive/${i + 1}.jpg`, {
+        //     method: 'GET',
+        //     responseType: 'blob'
+        // })
+        //     .then(resp => resp.blob())
+        //     .then(data => {
+        //
+        //     });
+        images[i].style.padding = '0';
+        images[i].setAttribute('src', `/assets/screenshots/progressive/${i + 1}.jpg`);
+    }
 };
